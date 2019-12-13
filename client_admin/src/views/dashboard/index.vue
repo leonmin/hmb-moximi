eaa4f04710e416cda98132c0b304038ca4d2fe15
liyao <244885878@qq.com>
2019-12-13 10:43:14 +0800

    'update'

--- client_admin/src/views/dashboard/index.vue
+++ client_admin/src/views/dashboard/index.vue
@@ -1,232 +1,175 @@
 <template>
-  <!--  用户列表-->
-  <div v-loading="loading" class="dashboard-container">
-    <!--    <div class="dashboard-text">name: {{ name }}</div>-->
-    <!--    <div class="dashboard-text">roles: <span v-for="role in roles" :key="role">{{ role }}</span></div>-->
-    <!--    页面标题-->
-    <!--    <div class="pageTitle">-->
-    <!--      <p>用户列表</p>-->
-    <!--      <div class="deliver"></div>-->
-    <!--    </div>-->
-    <!--   搜索栏-->
-    <div class="search clearfix">
-      <div class="searchItem">
-        <p>关键词</p>
-        <el-input
-          v-model="nameInput"
-          placeholder="用户名称/用户手机号"
-          style="width: 180px"
-          clearable
-        />
-      </div>
-      <div class="searchItem" style="margin-left: 20px">
-        <p>是否已成会员</p>
-        <el-select v-model="selectValue" placeholder="请选择">
-          <el-option :key="-1" label="全部" :value="-1" />
-          <el-option :key="1" label="是" :value="1" />
-          <el-option :key="0" label="否" :value="0" />
+  <div v-loading="loading" class="main">
+    <div class="title">用户列表</div>
+    <el-form :inline="true" :model="searchData" class="demo-form-inline" label-width="90px" style="margin-top: 30px">
+      <el-form-item label="关键字" style="margin-left: 5px">
+        <el-input v-model="searchData.searchKey" placeholder="用户名称\用户手机号" />
+      </el-form-item>
+      <el-form-item label="是否是会员">
+        <el-select v-model="searchData.isMember" placeholder="请选择" clearable>
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
         </el-select>
-      </div>
-      <el-button type="primary" class="searchBtn" @click="seachList">查询</el-button>
-    </div>
-    <!--   用户列表-->
-    <div class="tableList">
-      <el-table
-        :data="tableData"
-        border
-        style="width: 100%;min-height: 680px"
-      >
-        <el-table-column
-          prop="userName"
-          label="用户名称"
-        />
-        <el-table-column
-          prop="mobile"
-          label="用户手机号"
-          :formatter="mobileChange"
-        />
-        <el-table-column
-          prop="isMember"
-          label="是否已成会员"
-        />
-        <el-table-column
-          prop="memberEndTime"
-          label="会员到期日"
-        />
-        <el-table-column
-          prop="addTime"
-          label="注册时间"
-        />
-        <el-table-column
-          prop="puserName"
-          label="上级用户"
-        />
-        <el-table-column
-          prop="puserMobile"
-          label="上级手机号"
-        />
-        <el-table-column
-          prop="inviteUserCount"
-          label="下级用户数"
-        />
-        <el-table-column
-          label="操作"
-        >
-          <template v-slot="scope">
-            <el-button type="default" @click="lowerDetails(scope.row)" style="margin-top: -10px">查看</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </div>
-    <!--    分页-->
-    <div class="pageList clearfix">
-      <div class="pageination">
-        <p style="display: inline-block">共{{ totalPage }}页/{{ totalNum }}条数据</p>
-        <el-pagination
-          background
-          :current-page.sync="currentPage"
-          :page-size="pageSize"
-          layout="prev, pager, next, jumper"
-          :total="totalNum"
-          style="display: inline-block"
-          @size-change="handleSizeChange"
-          @current-change="currentChange"
-        />
-      </div>
-    </div>
-
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" style="margin-left: 30px" @click="loadList()">查询</el-button>
+        <el-button type="info" style="margin-left: 30px" @click="reset()">重置</el-button>
+      </el-form-item>
+    </el-form>
+    <!--表格-->
+    <el-table :data="tableData" style="width: 95%;margin-left: 40px;" border :height="fullHeight-280+'px'">
+      <el-table-column prop="userName" label="用户名称" min-width="150" show-overflow-tooltip />
+      <el-table-column prop="mobile" label="用户手机号" min-width="150" show-overflow-tooltip>
+        <template v-slot="scope">
+          <span>{{ scope.row.mobile | formatTel }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="isMember" label="是否已成会员" min-width="100" show-overflow-tooltip>
+        <template v-slot="scope">
+          <span>{{ scope.row.isMember?'是':'否' }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="memberEndTime" label="会员到期日" min-width="180" show-overflow-tooltip />
+      <el-table-column prop="addTime" label="注册时间" min-width="180" show-overflow-tooltip />
+      <el-table-column prop="puserName" label="上级用户" min-width="150" show-overflow-tooltip />
+      <el-table-column prop="puserMobile" label="上级手机号" min-width="150" show-overflow-tooltip>
+        <template v-slot="scope">
+          <span>{{ scope.row.puserMobile | formatTel }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="inviteUserCount" label="下级用户数" min-width="150" show-overflow-tooltip>
+        <template v-slot="scope">
+          <span style="cursor: pointer;color: #409EFF;" @click="look(scope.row)">{{ scope.row.inviteUserCount }}</span>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!--分页-->
+    <el-pagination
+      style="float: right;margin-top: 20px;margin-right: 40px"
+      :current-page="searchData.pageNum"
+      :page-sizes="[10,30,50,100,200]"
+      :page-size="searchData.pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import { getUserList } from '@/api/user'
 export default {
-  name: 'Dashboard',
-  computed: {
-    ...mapGetters([
-      'name',
-      'roles'
-    ])
-  },
-  mounted() {
-    this.initData()
-  },
-  data() {
-    return {
-      nameInput: '',
-      selectValue: -1,
-      searchKey: '',
-      totalPage: '',
-      tableData: [],
-      currentPage: 1, // 当前页码
-      totalNum: 0, // 总条数
-      pageSize: 10, // 每页的数据条数
-      loading: false
+  name: 'Index',
+  filters: {
+    /* 是否过期*/
+    outTime: function(data) {
+      if (data === 0 || data === '0') {
+        return '未过期'
+      } else {
+        return '已过期'
+      }
     }
   },
-  methods: {
-    // 初始化数据
-    initData() {
-      this.loading = true
-      const params = {
-        searchKey: this.nameInput,
-        isMember: this.selectValue,
-        pageNum: this.currentPage
-        // pageSize: this.pageSize
+  // 存放 数据
+  data: function() {
+    return {
+      loading: false,
+      fullHeight: document.documentElement.clientHeight, // 页面高度
+      options: [
+        {
+          value: true,
+          label: '是'
+        },
+        {
+          value: false,
+          label: '否'
+        }
+      ],
+      tableData: [], // 表格数据
+      searchData: { // 筛选的数据
+        pageNum: 1,
+        pageSize: 10,
+        searchKey: '', // 关键字
+        isMember: '' // 是否是会员
+      },
+      total: null, // 总数
+      isPaging: false// 是否是分页
+    }
+  },
+  watch: {
+    fullHeight(val) {
+      if (!this.timer) {
+        this.fullHeight = val
+        this.timer = true
+        const that = this
+        setTimeout(function() {
+          that.timer = false
+        }, 400)
       }
-      getUserList(params).then(res => {
-        this.tableData = res.data
-        this.totalNum = res.data.total
-        this.pageSize = res.data.size
-        this.currentPage = res.data.current
-        this.loading = false
-        this.totalPage = res.data.pages
+    }
+  },
+  mounted() {
+    const that = this
+    window.onresize = () => {
+      return (() => {
+        window.fullHeight = document.documentElement.clientHeight
+        that.fullHeight = window.fullHeight
+      })()
+    }
+    this.loadList()
+  },
+  methods: {
+    // 重置
+    reset() {
+      this.searchData = { // 筛选的数据
+        pageNum: 1,
+        pageSize: 10,
+        searchKey: '', // 关键字
+        isMember: '' // 是否是会员
+      }
+      this.loadList()
+    },
+    // 查看
+    look(row) {
+      this.$router.push({
+        path: 'lowerDetails',
+        query: { id: row.id }
       })
     },
-    // 查询列表
-    seachList() {
-      this.initData()
+    // 当前页码
+    handleSizeChange(val) {
+      this.searchData.pageSize = val
+      this.loadList()
     },
-    // 用户下级详情
-    lowerDetails(row) {
-      this.$router.push({
-        path: '/lowerDetails?id=',
-        query: {
-          id: row.id
+    // 当前页数
+    handleCurrentChange(val) {
+      this.isPaging = true
+      this.searchData.pageNum = val
+      this.loadList()
+    },
+    loadList() {
+      this.loading = true
+      if (!this.isPaging) {
+        this.searchData.pageNum = 1
+      }
+      getUserList(this.searchData).then(res => {
+        if (res.code === 0 || res.code === '0') {
+          this.total = res.data.total
+          this.tableData = res.data.records
+          this.loading = false
+          this.isPaging = false
         }
       })
-    },
-    // 页码改变
-    handleSizeChange() {},
-    currentChange(currentPage) {
-      this.currentPage = currentPage
-    },
-    // 手机脱敏
-    mobileChange(row, column) {
-      var changeMobile = row[column.property]
-      if (changeMobile !== null) {
-        var reg = /^(\d{3})\d{4}(\d{4})$/
-        return changeMobile.replace(reg, '$1****$2')
-      } else {
-        return false
-      }
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-  >>>.el-table .cell:first-child {
-    height: 30px !important;
-    line-height: 30px !important;
+<style scoped>
+  .title{
+    font-size: 22px;
+    margin-top: 20px;
+    margin-left: 40px;
+    font-weight: bold;
   }
-  .dashboard {
-    &-container {
-      margin: 30px;
-    }
-    &-text {
-      font-size: 30px;
-      line-height: 46px;
-    }
-  }
-  .tableList{
-    min-height: 650px;
-    margin-bottom: 10px;
-  }
-  .pageTitle{
-    font-size: 24px;
-    color: #333333;
-  }
-  .pageList{
-    font-size: 14px;
-  }
-  .pageination{
-    display: flex;
-    float: right;
-    margin-right: 50px;
-    align-items: center;
-  }
-  .deliver{
-    border-top: 1px solid #dedede;
-    margin: 20px 0;
-  }
-  .search{
-    margin-bottom: 20px;
-    display: flex;
-    align-items: center;
-  }
-  .searchItem{
-    float: left;
-  }
-  .searchItem>p{
-    display: inline-block;
-    font-size: 18px;
-    margin-right:10px;
-  }
-  .searchBtn{
-    width: 100px;
-    margin-left: 50px;
-  }
-
 </style>
--- client_admin/src/views/dashboard/userDetails.vue
+++ client_admin/src/views/dashboard/userDetails.vue
@@ -1,69 +1,41 @@
 <template>
-  <div v-loading="loading" class="userDetails">
-    <!--    页面标题-->
-    <!--    <div class="pageTitle">-->
-    <!--      <p>用户列表</p>-->
-    <!--      <div class="deliver"></div>-->
-    <!--    </div>-->
-    <!--   搜索栏-->
-    <div class="search clearfix">
-      <div class="searchItem">
-        <p>关键词</p>
-        <el-input
-          v-model="searchData.key"
-          placeholder="用户名称/用户手机号"
-          style="width: 180px"
-          clearable
-          @input="loadList()"
-        />
-      </div>
-      <el-button type="primary" class="searchBtn" @click="loadList()">查询</el-button>
-    </div>
-    <!-- 表格-->
-    <el-table
-      :data="tableData"
-      border
-      style="width: 100%;min-height: 680px"
-    >
-      <el-table-column
-        prop="userName"
-        label="用户名称"
-      />
-      <el-table-column
-        prop="mobile"
-        label="用户手机号"
-      >
+  <div v-loading="loading" class="main">
+    <div class="title">下级用户列表</div>
+    <el-form :inline="true" :model="searchData" class="demo-form-inline" label-width="90px" style="margin-top: 30px">
+      <el-form-item label="关键字" style="margin-left: 5px">
+        <el-input v-model="searchData.key" placeholder="用户名称\用户手机号" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" style="margin-left: 30px" @click="loadList()">查询</el-button>
+        <el-button type="info" style="margin-left: 30px" @click="reset()">重置</el-button>
+      </el-form-item>
+    </el-form>
+    <!--表格-->
+    <el-table :data="tableData" style="width: 95%;margin-left: 40px;" border :height="fullHeight-280+'px'">
+      <el-table-column prop="userName" label="用户名称" min-width="150" show-overflow-tooltip />
+      <el-table-column prop="mobile" label="用户手机号" min-width="180" show-overflow-tooltip>
         <template v-slot="scope">
           <span>{{ scope.row.mobile | formatTel }}</span>
         </template>
       </el-table-column>
-      <el-table-column
-        prop="addTime"
-        label="注册时间"
-      />
-      <el-table-column
-        prop="vipMember"
-        label="是否会员"
-      >
-        <template slot-scope="scope">
+      <el-table-column prop="addTime" label="注册时间" min-width="180" show-overflow-tooltip />
+      <el-table-column prop="vipMember" label="是否已成会员" min-width="150" show-overflow-tooltip>
+        <template v-slot="scope">
           <span>{{ scope.row.vipMember?'是':'否' }}</span>
         </template>
       </el-table-column>
     </el-table>
-    <!--    分页-->
-    <div class="pageList clearfix">
-      <!--分页-->
-      <el-pagination
-        style="float: right;margin-top: 20px;margin-right: 40px"
-        :current-page="searchData.pageNum"
-        :page-sizes="[10,30,50,100,200]"
-        :page-size="searchData.pageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-      />
-    </div>
+    <!--分页-->
+    <el-pagination
+      style="float: right;margin-top: 20px;margin-right: 40px"
+      :current-page="searchData.pageNum"
+      :page-sizes="[10,30,50,100,200]"
+      :page-size="searchData.pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
   </div>
 </template>
 
@@ -71,25 +43,56 @@
 import { partnerSubUserDetails } from '@/api/user'
 export default {
   name: 'UserDetails',
-  data() {
+  // 存放 数据
+  data: function() {
     return {
-      valueInput: '',
-      tableData: [],
-      searchData: {
+      loading: false,
+      fullHeight: document.documentElement.clientHeight, // 页面高度
+      tableData: [], // 表格数据
+      searchData: { // 筛选的数据
         pageNum: 1, // 当前页码
         pageSize: 10, // 每页的数据条数
         key: '',
         id: ''
       },
-      total: null,
-      loading: false
+      total: null, // 总数
+      isPaging: false// 是否是分页
+    }
+  },
+  watch: {
+    fullHeight(val) {
+      if (!this.timer) {
+        this.fullHeight = val
+        this.timer = true
+        const that = this
+        setTimeout(function() {
+          that.timer = false
+        }, 400)
+      }
     }
   },
   mounted() {
+    const that = this
+    window.onresize = () => {
+      return (() => {
+        window.fullHeight = document.documentElement.clientHeight
+        that.fullHeight = window.fullHeight
+      })()
+    }
     this.searchData.id = this.$route.query.id
     this.loadList()
   },
   methods: {
+    // 重置
+    reset() {
+      this.searchData = { // 筛选的数据
+        pageNum: 1, // 当前页码
+        pageSize: 10, // 每页的数据条数
+        key: '',
+        id: this.$route.query.id
+      }
+      this.loadList()
+    },
     // 当前页码
     handleSizeChange(val) {
       this.searchData.pageSize = val
@@ -97,17 +100,21 @@ export default {
     },
     // 当前页数
     handleCurrentChange(val) {
+      this.isPaging = true
       this.searchData.pageNum = val
       this.loadList()
     },
     loadList() {
       this.loading = true
+      if (!this.isPaging) {
+        this.searchData.pageNum = 1
+      }
       partnerSubUserDetails(this.searchData).then(res => {
-        if (res.code === 0) {
-          console.log(res)
+        if (res.code === 0 || res.code === '0') {
           this.total = res.data.total
           this.tableData = res.data.records
           this.loading = false
+          this.isPaging = false
         }
       })
     }
@@ -116,41 +123,10 @@ export default {
 </script>
 
 <style scoped>
-.userDetails{
-margin: 30px;
-}
-  .pageTitle{
-    font-size: 24px;
-    color: #333333;
+  .title{
+    font-size: 22px;
+    margin-top: 20px;
+    margin-left: 40px;
+    font-weight: bold;
   }
-.deliver{
-  border-top: 1px solid #dedede;
-  margin: 20px 0;
-}
-.search{
-  margin-bottom: 20px;
-  display: flex;
-  align-items: center;
-}
-.searchItem{
-  float: left;
-}
-.searchItem>p{
-  display: inline-block;
-  font-size: 18px;
-  margin-right:10px;
-}
-.searchBtn{
-  width: 100px;
-  margin-left: 50px;
-}
-.pageList{
-  font-size: 14px;
-}
-.pageination{
-  display: flex;
-  float: right;
-  margin-right: 50px;
-  align-items: center;
-}
 </style>
