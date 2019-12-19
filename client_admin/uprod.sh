271c53b996d0dcca3a5a00c51e9c56587372d484
saplmm <1046439010@qq.com>
2019-12-19 19:00:34 +0800

    no message

--- client_admin/uprod.sh
+++ client_admin/uprod.sh
@@ -2,3 +2,6 @@
 server="root@39.105.14.82"
 npm run build:prod
 scp -r dist/*  ${server}:/home/ai_assist/ai_admin/public/
+
+
+scp -r /Users/saplmm/Desktop/qrcode_for_gh_ca1418f4d13f_344.jpg  root@39.105.14.82:/home/ai_assist/ai_client/h5/
--- client_admin/vue.config.js
+++ client_admin/vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = defaultSettings.title || '格子衫数据平台' // page title
+const name = defaultSettings.title || '魔小秘' // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
