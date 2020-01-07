9ca88b88d3a1ba03ef70355c9c4c02438f163c08
saplmm <1046439010@qq.com>
2020-01-07 15:34:40 +0800

    增加测试环境的打包

--- client_admin/uprod.sh
+++ client_admin/uprod.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-server="root@39.105.14.82"
+#server="root@39.105.14.82"
+server="root@39.106.95.146"
 npm run build:prod
 scp -r dist/*  ${server}:/home/ai_assist/ai_admin/public/
